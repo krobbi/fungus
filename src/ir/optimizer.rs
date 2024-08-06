437dc6a48e3f54427ba3b1d5403b0d1e9a450976
krobbi <70552146+krobbi@users.noreply.github.com>
2024-08-06 20:05:12 +0100

    Add block merging optimization and remove broken jump threading optimization

--- src/ir/optimizer.rs
+++ src/ir/optimizer.rs
@@ -6,83 +6,45 @@ use super::{Exit, Program};
 
 /// Optimize a program.
 pub fn optimize_program(program: &mut Program) {
-    while thread_jumps(program) {}
-}
+    loop {
+        let mut optimized = false;
+        merge_blocks(program, &mut optimized);
 
-/// Redirect exits that target jumps to the jump's target.
-fn thread_jumps(program: &mut Program) -> bool {
-    /// Redirect a label if it exists in a map of redirects and set a flag.
-    fn redirect_label(label: &mut Label, redirects: &HashMap<Label, Label>, redirected: &mut bool) {
-        if redirects.contains_key(label) {
-            *label = *redirects.get(label).unwrap();
-            *redirected = true;
+        if !optimized {
+            break;
         }
-    }
-
-    let mut redirects = HashMap::new();
-
-    for (&label, block) in &program.blocks {
-        if let Exit::Jump(target) = block.exit {
-            if target != label && block.instructions.is_empty() {
-                redirects.insert(label, target);
-            }
-        }
-    }
-
-    if redirects.is_empty() {
-        return false;
-    }
-
-    let mut redirected = false;
-
-    for exit in program.blocks.values_mut().map(|block| &mut block.exit) {
-        match exit {
-            Exit::Jump(label) => redirect_label(label, &redirects, &mut redirected),
-            Exit::Random(right, down, left, up) => {
-                redirect_label(right, &redirects, &mut redirected);
-                redirect_label(down, &redirects, &mut redirected);
-                redirect_label(left, &redirects, &mut redirected);
-                redirect_label(up, &redirects, &mut redirected);
-            }
-            Exit::If { non_zero, zero } => {
-                redirect_label(non_zero, &redirects, &mut redirected);
-                redirect_label(zero, &redirects, &mut redirected);
-            }
-            Exit::End => (),
-        }
-    }
-
-    if redirected {
-        remove_unreachable_blocks(program);
-        true
-    } else {
-        false
     }
 }
 
-/// Remove unreachable basic blocks.
-fn remove_unreachable_blocks(program: &mut Program) {
-    let mut pending_labels = vec![Label::default()];
-    let mut reachable_labels = HashSet::new();
+/// Merge basic blocks with one entry point from a jump into their predecessor.
+fn merge_blocks(program: &mut Program, optimized: &mut bool) {
+    let mut successor_predecessors = HashMap::new();
+    let mut invalid_successors = HashSet::new();
+    invalid_successors.insert(Label::default());
 
-    while let Some(label) = pending_labels.pop() {
-        if reachable_labels.contains(&label) {
+    for (&predecessor, block) in &program.blocks {
+        if let Exit::Jump(successor) = block.exit {
+            if successor != predecessor && !successor_predecessors.contains_key(&successor) {
+                successor_predecessors.insert(successor, predecessor);
+            } else {
+                invalid_successors.insert(successor);
+            }
+        } else {
+            for successor in block.exit.exit_labels() {
+                invalid_successors.insert(successor);
+            }
+        }
+    }
+
+    for (successor, predecessor) in successor_predecessors {
+        if invalid_successors.contains(&successor) || !program.blocks.contains_key(&predecessor) {
             continue;
         }
 
-        pending_labels.append(&mut program.blocks.get(&label).unwrap().exit.exit_labels());
-        reachable_labels.insert(label);
-    }
-
-    let mut unreachable_labels = HashSet::new();
-
-    for &label in program.blocks.keys() {
-        if !reachable_labels.contains(&label) {
-            unreachable_labels.insert(label);
-        }
-    }
-
-    for label in unreachable_labels {
-        program.blocks.remove(&label);
+        let mut successor = program.blocks.remove(&successor).unwrap();
+        let predecessor = program.blocks.get_mut(&predecessor).unwrap();
+        predecessor.instructions.append(&mut successor.instructions);
+        predecessor.exit = successor.exit;
+        *optimized = true;
     }
 }
