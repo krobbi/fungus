8a3412d03d985ebdb160d602e0a62238c97a7e27
krobbi <70552146+krobbi@users.noreply.github.com>
2025-05-21 11:58:27 +0100

    Revert to hello world program

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,4 @@
-mod config;
-mod error;
-mod ir;
-mod playfield;
-mod pointer;
-
-use std::fs;
-
-use config::Config;
-use error::Result;
-use ir::Program;
-
-/// Run Fungus or exit with an error.
+/// Runs Fungus.
 fn main() {
-    run_fungus().unwrap_or_else(|error| error.exit());
-}
-
-/// Run Fungus and get a result.
-fn run_fungus() -> Result<()> {
-    let config = Config::new()?;
-    let program = Program::new(&fs::read_to_string(config.path())?)?;
-
-    if config.dump() {
-        program.dump();
-    } else {
-        program.interpret();
-    }
-
-    Ok(())
+    println!("Hello, Fungus!");
 }
