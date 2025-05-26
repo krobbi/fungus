46f9b5062446e96747ca0d7b9dc581d055024ae3
krobbi <70552146+krobbi@users.noreply.github.com>
2025-05-27 00:00:05 +0100

    Parse stack manipulation commands

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -15,6 +15,18 @@ pub enum Instruction {
     /// An instruction to apply a unary operation to the stack.
     /// `[...][rhs]` -> `[...][op rhs]`
     Unary(UnOp),
+
+    /// An instruction to duplicate the top value of the stack.
+    /// `[...][value]` -> `[...][value][value]`
+    Duplicate,
+
+    /// An instruction to swap the top two values of the stack.
+    /// `[...][under][top]` -> `[...][top][under]`
+    Swap,
+
+    /// An instruction to pop the top value from the stack.
+    /// `[...][value]` -> `[...]`
+    Pop,
 }
 
 impl Display for Instruction {
@@ -23,6 +35,9 @@ impl Display for Instruction {
             Self::Push(e) => write!(f, "{:8}{e}", "push"),
             Self::Binary(o) => write!(f, "{:8}{o}", "binary"),
             Self::Unary(o) => write!(f, "{:8}{o}", "unary"),
+            Self::Duplicate => f.write_str("dup"),
+            Self::Swap => f.write_str("swap"),
+            Self::Pop => f.write_str("pop"),
         }
     }
 }
