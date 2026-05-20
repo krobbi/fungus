07459dff6e6a4ce7d7a3e9c9c92ce8877f7fba76
krobbi <70552146+krobbi@users.noreply.github.com>
2026-05-20 12:10:34 +0100

    Optimize branches

--- src/optimize/steps/mod.rs
+++ src/optimize/steps/mod.rs
@@ -1,5 +1,6 @@
 mod fold_exprs;
 mod merge_basic_blocks;
+mod optimize_branches;
 mod remove_unreachable_basic_blocks;
 mod replace_peepholes;
 mod thread_jumps;
@@ -13,9 +14,9 @@ use super::context::Context;
 pub fn run_all_steps(cfg: &mut Cfg, ctx: &mut Context) {
     merge_basic_blocks::run_step(cfg, ctx);
     thread_jumps::run_step(cfg, ctx);
+    optimize_branches::run_step(cfg, ctx);
+    unroll_print_loops::run_step(cfg, ctx);
     remove_unreachable_basic_blocks::run_step(cfg, ctx);
     replace_peepholes::run_step(cfg, ctx);
     fold_exprs::run_step(cfg, ctx);
-    unroll_print_loops::run_step(cfg, ctx);
-    remove_unreachable_basic_blocks::run_step(cfg, ctx);
 }
---
+++ src/optimize/steps/optimize_branches.rs
@@ -0,0 +1,70 @@
+use crate::{
+    cfg::{Cfg, Expr, Instruction, Terminator, UnOp},
+    optimize::context::Context,
+};
+
+/// Optimizes [`BasicBlock`][crate::cfg::BasicBlock]s with branch
+/// [`Terminator`]s.
+pub fn run_step(cfg: &mut Cfg, ctx: &mut Context) {
+    for basic_block in cfg.basic_blocks_mut_unstable() {
+        let Terminator::Branch(then_label, else_label) = basic_block.terminator else {
+            continue;
+        };
+
+        if then_label == else_label {
+            basic_block.instructions.push(Instruction::Pop);
+            basic_block.terminator = Terminator::Jump(then_label);
+            ctx.mark_change();
+            continue;
+        }
+
+        match basic_block.instructions.last() {
+            Some(Instruction::Push(expr)) if let Some(value) = expr.eval_const() => {
+                let label = if value.is_non_zero() {
+                    then_label
+                } else {
+                    else_label
+                };
+
+                basic_block.instructions.push(Instruction::Pop);
+                basic_block.terminator = Terminator::Jump(label);
+                ctx.mark_change();
+            }
+            Some(Instruction::Push(expr))
+                if let Some((expr, is_negated)) = fold_condition(expr) =>
+            {
+                basic_block.instructions.pop();
+                basic_block.instructions.push(Instruction::Push(expr));
+
+                if is_negated {
+                    basic_block.terminator = Terminator::Branch(else_label, then_label);
+                }
+
+                ctx.mark_change();
+            }
+            Some(Instruction::Unary(UnOp::Negate | UnOp::Bool)) => {
+                basic_block.instructions.pop();
+                ctx.mark_change();
+            }
+            Some(Instruction::Unary(UnOp::Not)) => {
+                basic_block.instructions.pop();
+                basic_block.terminator = Terminator::Branch(else_label, then_label);
+                ctx.mark_change();
+            }
+            _ => (),
+        }
+    }
+}
+
+/// Returns a more optimal equivalent of a condition [`Expr`] and whether it is
+/// negated.
+fn fold_condition(expr: &Expr) -> Option<(Expr, bool)> {
+    match expr {
+        Expr::Unary(UnOp::Negate | UnOp::Bool, rhs) => Some((*rhs.clone(), false)),
+        Expr::Unary(UnOp::Not, rhs) => Some((*rhs.clone(), true)),
+        Expr::Sequence(prefix, expr) if let Some((expr, is_negated)) = fold_condition(expr) => {
+            Some((Expr::Sequence(prefix.clone(), Box::new(expr)), is_negated))
+        }
+        _ => None,
+    }
+}
